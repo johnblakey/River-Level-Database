12e2c87bc251dc3f02de3e2c8ac6e6076449f3a0
johnblakey <blakeyjo@oregonstate.edu>
2017-12-10 12:47:01 -0700

    sqlite nodejs displays multiple rows cleaned up formatting and naming

--- sqliteConnectNodejs/connect.js
+++ sqliteConnectNodejs/connect.js
@@ -12,19 +12,19 @@ var url = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=09058000,06
 // json response
 var usgsJSON;
 
-https.get(url, function(res){
+https.get(url, function(response){
     var body = '';
 
-    res.on('data', function(chunk){
+    response.on('data', function(chunk){
         body += chunk;
     });
 
-    res.on('end', function(){
+    response.on('end', function(){
         usgsJSON = JSON.parse(body);
         console.log("Got a response: ", usgsJSON.value.timeSeries[1].values[0].value[0].value);
     });
-}).on('error', function(e){
-    console.log("Got an error: ", e);
+}).on('error', function(err){
+    console.error(err.message);
 });
 
 // need a pre-populated river list (mvp complete with 2 rivers)
@@ -32,34 +32,53 @@ https.get(url, function(res){
 // parse usgs json and upload to db
 //json.forEach(function);
 
-/** sqlite db */
+/** sqlite db modify with json data */
 // .verbose() can be removed to reduce the stack trace
 const sqlite3 = require('sqlite3').verbose();
 
-// open the database (note the hardcoded location of the db)
-let db = new sqlite3.Database('../levels.db', sqlite3.OPEN_READWRITE, (err) => {
+
+/* open database from local file */
+let dbSource = "../levels.db";
+let db = new sqlite3.Database(dbSource, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Connected to the levels database.');
+    console.log('Open connection to database:', dbSource);
 });
 
-// execute each sql query in order
+/* execute each sql query in order */
 db.serialize(function() {
     let sql;    // sql query variable
 
-    /* see all levels rows */
+    /* display all levels rows */
+    sql = "SELECT * FROM levels;";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+        }
+        rows.forEach((row) => {
+            console.log(row);
+        });
+    });
+
+    /* display one levels row */
     sql = "SELECT * FROM levels;";
     db.get(sql, function(err, rows) {
-        console.log(err);
+        if (err) {
+            console.error(err.message);
+        }
         console.log(rows);
     });
 
-    /* see all rivers rows */
+    /* display all rivers rows */
     sql = "SELECT * FROM rivers;";
-    db.get(sql, function(err, rows) {
-        console.log(err);
-        console.log(rows);
+    db.all(sql, [], function(err, rows) {
+        if (err) {
+            console.error(err.message);
+        }
+        rows.forEach((row) => {
+            console.log(row);
+        });
     });
 
 });
@@ -69,5 +88,5 @@ db.close((err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Close the database connection.');
+    console.log('Close connection to database:', dbSource);
 });
