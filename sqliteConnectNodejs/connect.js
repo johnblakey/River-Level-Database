8279e2db813782d924ad0613d35c3579ca09a981
johnblakey <blakeyjo@oregonstate.edu>
2017-12-10 18:50:12 -0700

    Insert test data into db db allows riverId foreign key to be null for testing

--- sqliteConnectNodejs/connect.js
+++ sqliteConnectNodejs/connect.js
@@ -4,89 +4,125 @@
  * // refactor into separate files eventually
  */
 
-/** json from waterservices.usgs.gov */
+// https package for getting json data
 const https = require('https');
 
-var url = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=09058000,06719505&parameterCd=00060&siteStatus=all"
-
-// json response
-var usgsJSON;
-
-https.get(url, function(response){
-    var body = '';
-
-    response.on('data', function(chunk){
-        body += chunk;
-    });
-
-    response.on('end', function(){
-        usgsJSON = JSON.parse(body);
-        console.log("Got a response: ", usgsJSON.value.timeSeries[1].values[0].value[0].value);
-    });
-}).on('error', function(err){
-    console.error(err.message);
-});
-
-// need a pre-populated river list (mvp complete with 2 rivers)
-
-// parse usgs json and upload to db
-//json.forEach(function);
-
-/** sqlite db modify with json data */
-// .verbose() can be removed to reduce the stack trace
+// sqlite3 to connect to db, .verbose() can be removed to reduce the stack trace
 const sqlite3 = require('sqlite3').verbose();
 
+/** json from waterservices.usgs.gov */
+function getData() {
+    var url = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=09058000,06719505&parameterCd=00060&siteStatus=all"
 
-/* open database from local file */
-let dbSource = "../levels.db";
-let db = new sqlite3.Database(dbSource, sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Open connection to database:', dbSource);
-});
+    // json response variable
+    var usgsJSON;
 
-/* execute each sql query in order */
-db.serialize(function() {
-    let sql;    // sql query variable
+    https.get(url, function(response){
+        var body = '';
 
-    /* display all levels rows */
-    sql = "SELECT * FROM levels;";
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            console.error(err.message);
-        }
-        rows.forEach((row) => {
-            console.log(row);
+        response.on('data', function(chunk){
+            body += chunk;
         });
-    });
 
-    /* display one levels row */
-    sql = "SELECT * FROM levels;";
-    db.get(sql, function(err, rows) {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log(rows);
-    });
-
-    /* display all rivers rows */
-    sql = "SELECT * FROM rivers;";
-    db.all(sql, [], function(err, rows) {
-        if (err) {
-            console.error(err.message);
-        }
-        rows.forEach((row) => {
-            console.log(row);
+        response.on('end', function(){
+            usgsJSON = JSON.parse(body);
+            dbUtility(usgsJSON);
         });
+    }).on('error', function(err){
+        console.error(err.message);
+    });
+}
+
+function insertValues(db, usgs) {
+    /* execute each sql query in order */
+    db.serialize(function() {
+        let sql;    // sql query variable
+
+        console.log("JSON received, one test level value: ", usgs.value.timeSeries[1].values[0].value[0].value);
+        // iterate through given series
+        for (var i in usgs.value.timeSeries) {
+            /* insert new json data */
+            var level = usgs.value.timeSeries[i].values[0].value[0].value;
+            var dateTime = usgs.value.timeSeries[i].values[0].value[0].dateTime;
+            console.log(level);
+            console.log(dateTime);
+
+            //TODO turn into function, add logic to prevent same data added, webstorm hints
+            /* insert data into table */
+            db.run(`INSERT INTO levels (levelValue, dateTime) VALUES (?,?)`, [level, dateTime], function(err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+                // get the last insert id
+                console.log(`A row has been inserted with rowid ${this.lastID}`);
+            });
+
+        }
+
+        /** debugging display of results of tables */
+        /* display all levels rows */
+        sql = "SELECT * FROM levels;";
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                console.error(err.message);
+            }
+            rows.forEach((row) => {
+                console.log(row);
+            });
+        });
+
+        /* display one levels row */
+        sql = "SELECT * FROM levels;";
+        db.get(sql, function(err, rows) {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log(rows);
+        });
+
+        /* display all rivers rows */
+        sql = "SELECT * FROM rivers;";
+        db.all(sql, [], function(err, rows) {
+            if (err) {
+                console.error(err.message);
+            }
+            rows.forEach((row) => {
+                console.log(row);
+            });
+        });
+
     });
 
-});
+    /* Close database */
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Close connection to database');
+    });
 
-/* Close database */
-db.close((err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Close connection to database:', dbSource);
-});
+}
+
+/** sqlite db modify with json data */
+function dbUtility(usgs) {
+
+    // need a pre-populated river list (mvp complete with 2 rivers)
+
+    /* open database from local file */
+    let dbSource = "../levels.db";
+    let db = new sqlite3.Database(dbSource, sqlite3.OPEN_READWRITE, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Open connection to database:', dbSource);
+
+        insertValues(db, usgs);
+    });
+
+}
+
+// main start
+getData();
+
+
+
