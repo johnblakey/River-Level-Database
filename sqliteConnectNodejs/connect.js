01410da87184236aef35c7ee81e31b66dd2e38f5
johnblakey <blakeyjo@oregonstate.edu>
2017-12-12 11:38:16 -0700

    close and debugging function not used trying to determine how to capture a for loop of async calls

--- sqliteConnectNodejs/connect.js
+++ sqliteConnectNodejs/connect.js
@@ -37,56 +37,6 @@ function getData() {
     });
 }
 
-function insertValues(db, usgs) {
-    /* execute each sql query in order */
-    db.serialize(function() {
-        let sql;    // sql query variable
-
-        console.log("JSON received, one test level value: ", usgs.value.timeSeries[1].values[0].value[0].value);
-        // iterate through given series
-        for (var i in usgs.value.timeSeries) {
-            /* insert new json data */
-            var level = usgs.value.timeSeries[i].values[0].value[0].value;
-            var dateTime = usgs.value.timeSeries[i].values[0].value[0].dateTime;
-            var siteCode = usgs.value.timeSeries[i].sourceInfo.siteCode[0].value;
-            console.log("Iteration:", i);
-            console.log(level);
-            console.log(dateTime);
-            console.log(siteCode);
-
-            (function(counter) {
-                var level = usgs.value.timeSeries[counter].values[0].value[0].value;
-                var dateTime = usgs.value.timeSeries[counter].values[0].value[0].dateTime;
-                var siteCode = usgs.value.timeSeries[counter].sourceInfo.siteCode[0].value;
-
-                //TODO turn into function, add logic to prevent same data added, webstorm hints
-
-                /* grab correct foreign key for insert below */
-                let sql = `SELECT RiverId FROM rivers WHERE siteCode = ?`;
-                db.get(sql, [siteCode], level, dateTime, (err, row) => {
-                    if (err) {
-                        console.error(err.message);
-                    }
-                    console.log("RiverId", row.RiverId);
-                    console.log("level", level);
-                    console.log("dateTime", dateTime);
-
-                    /* insert data into table */
-                    db.run(`INSERT INTO levels (levelValue, dateTime, riverId) VALUES (?,?,?)`, [level, dateTime, row.RiverId], function(err) {
-                        if (err) {
-                            console.error(err.message);
-                        }
-                        // get the last insert id
-                        console.log(`A row has been inserted into levels: levelId = ${this.lastID}`);
-                    });
-                });
-
-
-            })(i);
-        }
-    });
-}
-
 /** sqlite db modify with json data */
 function dbUtility(usgs) {
 
@@ -105,27 +55,77 @@ function dbUtility(usgs) {
     });
 }
 
+function dbInsert(db, siteCode, level, dateTime) {
+    /* grab correct foreign key for insert below */
+    let sql = `SELECT RiverId FROM rivers WHERE siteCode = ?`;
+    db.get(sql, [siteCode], level, dateTime, (err, row) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log("RiverId", row.RiverId);
+        console.log("level", level);
+        console.log("dateTime", dateTime);
+
+        /* insert data into table */
+        db.run(`INSERT INTO levels (levelValue, dateTime, riverId) VALUES (?,?,?)`, [level, dateTime, row.RiverId], function(err) {
+            if (err) {
+                console.error(err.message);
+            }
+            // get the last insert id
+            console.log(`A row has been inserted into levels: levelId = ${this.lastID}`);
+        });
+    });
+}
+
+function close(db) {
+    /** debugging display of results of tables */
+    console.log("Display current tables =========================");
+    /* display all levels rows */
+    sql = "SELECT * FROM levels;";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+        }
+        rows.forEach((row) => {
+            console.log(row);
+        });
+    });
+    /* Close database */
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Close connection to database');
+    });
+}
+
+function insertValues(db, usgs) {
+    /* execute each sql query in order */
+    db.serialize(function() {
+        console.log("JSON received, one test level value: ", usgs.value.timeSeries[1].values[0].value[0].value);
+        // iterate through given series
+        for (var i in usgs.value.timeSeries) {
+            /* insert new json data */
+            var level = usgs.value.timeSeries[i].values[0].value[0].value;
+            var dateTime = usgs.value.timeSeries[i].values[0].value[0].dateTime;
+            var siteCode = usgs.value.timeSeries[i].sourceInfo.siteCode[0].value;
+            console.log("Iteration:", i);
+            console.log(level);
+            console.log(dateTime);
+            console.log(siteCode);
+
+            (function(counter) {
+                var level = usgs.value.timeSeries[counter].values[0].value[0].value;
+                var dateTime = usgs.value.timeSeries[counter].values[0].value[0].dateTime;
+                var siteCode = usgs.value.timeSeries[counter].sourceInfo.siteCode[0].value;
+
+                // logic to close db after loop is complete
+                dbInsert(db, siteCode, level, dateTime);
+
+            })(i);
+        }
+    });
+}
+
 // main start
 updateDbStart();
-
-
-/** debugging display of results of tables */
-//console.log("Display current tables =========================");
-/* display all levels rows */
-/*sql = "SELECT * FROM levels;";
-db.all(sql, [], (err, rows) => {
-    if (err) {
-        console.error(err.message);
-    }
-    rows.forEach((row) => {
-        console.log(row);
-    });
-});
-*/
-/* Close database */
-/*db.close((err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Close connection to database');
-});*/
