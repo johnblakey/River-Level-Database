c1f713b680a39f7670798ca3de2c308e1c5b6188
johnblakey <blakeyjo@oregonstate.edu>
2017-12-13 23:46:29 -0700

    basic nodejs webserver, WIP

---
+++ webserverHelloWorld/helloWorld.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jb on 12/9/17.
+ * https://stackoverflow.com/questions/8017674/nodejs-server-not-accessible-from-outside
+ */
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(3000, "0.0.0.0");
+console.log('Server running at http://0.0.0.0:3000/');
