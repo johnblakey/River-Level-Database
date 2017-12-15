fa6f5fe27bb82e0e7b9da642812896631a9531aa
johnblakey <blakeyjo@oregonstate.edu>
2017-12-14 23:44:18 -0700

    Change name

--- webserver/helloWorld.js
+++ webserver/helloWorld.js
@@ -7,6 +7,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello sdcommander!\n');
 }).listen(3000, "0.0.0.0");
 console.log('Server running at http://0.0.0.0:3000/');
