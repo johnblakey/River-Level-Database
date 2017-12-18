261ad9604af00cc774418fa68da6237c2ca671b9
johnblakey <blakeyjo@oregonstate.edu>
2017-12-17 23:44:00 -0700

    Changed date logging and verbose debug logging

--- nodejsInsert/insert.sh
+++ nodejsInsert/insert.sh
@@ -4,11 +4,11 @@
 # Description: run insert.js to insert new values into 
 # sqlite database on the Raspberry Pi.
 
-now="$(date +'%d/%m/%Y_%H-%M-%S')"
+now="$(date +'%m/%d/%Y_%H-%M-%S')"
 
 # insert river level values into sqlite db
 # For debugging change the '>' to '>>' record all entries in the file
-nodejs ~/Development/Production/River-Level-Database/nodejsInsert/insert.js >> /home/jb/Logs/insert.log 
+nodejs ~/Development/Production/River-Level-Database/nodejsInsert/insert.js > /home/jb/Logs/insert.log 
 
 printf '%s\n' "--------------------------------------------------" >> /home/jb/Logs/insert.log
 printf "Completed levels insertion at $now\n\n" >> /home/jb/Logs/insert.log
